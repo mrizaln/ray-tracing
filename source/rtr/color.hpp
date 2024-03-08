dc7fd862fbdcccd7ec1b45b5497b8cfbbc31a478
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2024-03-08 23:41:14 +0700

    Change Color to uses alias instead of its own type

--- source/rtr/color.hpp
+++ source/rtr/color.hpp
@@ -1,72 +1,97 @@
 #pragma once
 
+#include "rtr/vec.hpp"
 namespace rtr
 {
 
-    template <typename T = double>
-    struct Color
+    namespace v1_standalone
     {
-        union
+        template <typename T = double>
+        struct Color
         {
-            T r;
-            T h;
-            T x;
+            union
+            {
+                T r;
+                T h;
+                T x;
+            };
+            union
+            {
+                T g;
+                T s;
+                T y;
+            };
+            union
+            {
+                T b;
+                T v;
+                T z;
+            };
         };
-        union
-        {
-            T g;
-            T s;
-            T y;
-        };
-        union
-        {
-            T b;
-            T v;
-            T z;
-        };
-    };
 
-    template <typename T = double>
-    struct Color4
+        template <typename T = double>
+        struct Color4
+        {
+            union
+            {
+                T r;
+                T h;
+                T x;
+            };
+            union
+            {
+                T g;
+                T s;
+                T y;
+            };
+            union
+            {
+                T b;
+                T v;
+                T z;
+            };
+            union
+            {
+                T a;
+                T w;
+            };
+        };
+
+        template <typename To, typename From>
+        Color<To> colorCast(const Color<From>& from, From fmin, From fmax, To tmin, To tmax)
+        {
+            const auto fromScale = fmax - fmin;
+            const auto toScale   = tmax - tmin;
+
+            const auto cast = [&](auto value) { return static_cast<To>((value - fmin) / fromScale * toScale + tmin); };
+
+            return {
+                .r = cast(from.r),
+                .g = cast(from.g),
+                .b = cast(from.b),
+            };
+        }
+    }
+
+    inline namespace v2_alias
     {
-        union
-        {
-            T r;
-            T h;
-            T x;
-        };
-        union
-        {
-            T g;
-            T s;
-            T y;
-        };
-        union
-        {
-            T b;
-            T v;
-            T z;
-        };
-        union
-        {
-            T a;
-            T w;
-        };
-    };
+        template <typename T = double>
+        using Color = rtr::Vec<T, 3>;
 
-    template <typename To, typename From>
-    Color<To> colorCast(const Color<From>& from, From fmin, From fmax, To tmin, To tmax)
-    {
-        const auto fromScale = fmax - fmin;
-        const auto toScale   = tmax - tmin;
+        template <typename To, typename From>
+        Color<To> colorCast(const Color<From>& from, From fmin, From fmax, To tmin, To tmax)
+        {
+            const auto fromScale = fmax - fmin;
+            const auto toScale   = tmax - tmin;
 
-        const auto cast = [&](auto value) { return static_cast<To>((value - fmin) / fromScale * toScale + tmin); };
+            const auto cast = [&](auto value) { return static_cast<To>((value - fmin) / fromScale * toScale + tmin); };
 
-        return {
-            .r = cast(from.r),
-            .g = cast(from.g),
-            .b = cast(from.b),
-        };
+            return {
+                cast(from.x()),
+                cast(from.y()),
+                cast(from.z()),
+            };
+        }
     }
 
 }
